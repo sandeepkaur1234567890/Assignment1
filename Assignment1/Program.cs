4e41b78516d3627d66c1f96ade7d658bd1e47f99
sandeepkaur1234567890 <78116974+sandeepkaur1234567890@users.noreply.github.com>
2021-09-20 21:44:48 -0400

    commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the number of cards to pick: ");
+			string line = Console.ReadLine();
+			if (int.TryParse(line, out int numCards))
+			{
+				foreach (var card in CardPicker.PickSomeCards(numCards))
+				{
+					Console.WriteLine(card);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Please enter a valid number.");
+			}
+		}
+	}
+
+	public static class SubsequenceFinder
+	{
+		/// <summary>
+		/// Determines whether a list of integers is a subsequence of another list of integers
+		/// </summary>
+		/// <param name="seq">The main sequence of integers.</param>
+		/// <param name="subseq">The potential subsequence.</param>
+		/// <returns>True if subseq is a subsequence of seq and false otherise.</returns>
+		public static bool IsValidSubsequeuce(List<int> seq, List<int> subseq)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class CardPicker
+	{
+		static Random random = new Random(1);
+		/// <summary>
+		/// Picks a random (with replacement) number of cards.
+		/// </summary>
+		/// <param name="numCards">The number of cards to choose at random.</param>
+		/// <returns>An array of strings where each string represents a card.</returns>
+		public static string[] PickSomeCards(int numCards)
+		{
+			// Use RandomValue() & RandomSuit() to help you here
+			throw new NotImplementedException();
+		}
+		/// <summary>
+		/// Chooses a random value for a card (Ace, 2, 3, ... , Queen, King)
+		/// </summary>
+		/// <returns>A string that represents the value of a card</returns>
+		private static string RandomValue()
+		{
+			throw new NotImplementedException();
+		}
+		/// <summary>
+		/// Chooses a random suit for a card (Clubs, Diamonds, Hearts, Spades)
+		/// </summary>
+		/// <returns>A string that represents the suit of a card.</returns>
+		private static string RandomSuit()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
